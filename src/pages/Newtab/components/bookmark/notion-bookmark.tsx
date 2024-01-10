c4c692b407a87d213df9f51b5519215685366efa
Viet Hoang <ngviethoang0212@gmail.com>
2024-01-10 10:56:06 +0700

    update view table

--- src/pages/Newtab/components/bookmark/notion-bookmark.tsx
+++ src/pages/Newtab/components/bookmark/notion-bookmark.tsx
@@ -9,11 +9,42 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { notion } from '@/lib/notion';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import secrets from 'secrets';
 import UrlItem from './url-item';
 import { Switch } from '@/components/ui/switch';
 import { Label } from '@/components/ui/label';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+const getFilteredPages = (
+  pages: any[],
+  selectedTags: any[],
+  orFilter: boolean
+) => {
+  if (selectedTags.length) {
+    return pages.filter((page) => {
+      const pageTags = (page.properties.Tags as any).multi_select;
+      if (orFilter) {
+        return selectedTags.some((tag) =>
+          pageTags.find((t: any) => t.id === tag.id)
+        );
+      } else {
+        return selectedTags.every((tag) =>
+          pageTags.find((t: any) => t.id === tag.id)
+        );
+      }
+    });
+  } else {
+    return pages;
+  }
+};
 
 export default function NotionBookmark() {
   const [pages, setPages] = useState<any[]>([]);
@@ -22,16 +53,20 @@ export default function NotionBookmark() {
   const [allTags, setAllTags] = useState<any[]>([]);
   const [selectedTags, setSelectedTags] = useState<any[]>([]);
   const [orFilter, setOrFilter] = useState(true);
+  const [viewMode, setViewMode] = useState<'table' | 'list'>('list');
+
+  const loadPages = useCallback(async (nextCursor: string | null) => {
+    const response = await notion.databases.query({
+      database_id: secrets.NOTION_BOOKMARK_DATABASE_ID,
+      start_cursor: nextCursor || undefined,
+    });
+    setPages((prev) => [...prev, ...response.results]);
+    if (response.has_more) {
+      loadPages(response.next_cursor);
+    }
+  }, []);
 
   useEffect(() => {
-    const loadPages = async () => {
-      const response = await notion.databases.query({
-        database_id: secrets.NOTION_BOOKMARK_DATABASE_ID,
-      });
-      setPages(response.results);
-      setFilteredPages(response.results);
-    };
-
     const loadDatabase = async () => {
       const response = await notion.databases.retrieve({
         database_id: secrets.NOTION_BOOKMARK_DATABASE_ID,
@@ -40,30 +75,13 @@ export default function NotionBookmark() {
       setAllTags((response.properties.Tags as any).multi_select.options);
     };
 
-    loadPages();
+    loadPages(null);
     loadDatabase();
   }, []);
 
   useEffect(() => {
-    if (selectedTags.length) {
-      setFilteredPages(
-        pages.filter((page) => {
-          const pageTags = (page.properties.Tags as any).multi_select;
-          if (orFilter) {
-            return selectedTags.some((tag) =>
-              pageTags.find((t: any) => t.id === tag.id)
-            );
-          } else {
-            return selectedTags.every((tag) =>
-              pageTags.find((t: any) => t.id === tag.id)
-            );
-          }
-        })
-      );
-    } else {
-      setFilteredPages(pages);
-    }
-  }, [selectedTags, orFilter]);
+    setFilteredPages(getFilteredPages(pages, selectedTags, orFilter));
+  }, [pages, selectedTags, orFilter]);
 
   return (
     <div className="mt-6">
@@ -77,6 +95,16 @@ export default function NotionBookmark() {
       </a>
       {!!database && (
         <div className="flex gap-2 items-center my-2">
+          <div className="flex items-center space-x-2 mr-4">
+            <Switch
+              id="view-mode"
+              checked={viewMode === 'list'}
+              onCheckedChange={(val) => setViewMode(val ? 'list' : 'table')}
+            />
+            <Label htmlFor="view-mode">
+              {viewMode === 'list' ? 'List' : 'Table'}
+            </Label>
+          </div>
           <div className="flex items-center space-x-2 mr-4">
             <Switch
               id="or-filter"
@@ -114,32 +142,89 @@ export default function NotionBookmark() {
                 setSelectedTags((prev) => prev.filter((t) => t.id !== id));
               }}
               className="cursor-pointer"
+              style={{ backgroundColor: color }}
             >
               {name}
             </Badge>
           ))}
         </div>
       )}
-      <div className="flex flex-wrap gap-1 mt-4 gap-y-5">
-        {filteredPages.map(
-          ({
-            id,
-            properties: {
-              Link: { url },
-              Name: { title },
-              Tags,
-            },
-          }) => {
-            return (
-              <UrlItem
-                key={id}
-                url={url}
-                title={title.map(({ plain_text }: any) => plain_text).join(' ')}
-              />
-            );
-          }
-        )}
-      </div>
+      {viewMode === 'list' && (
+        <div className="flex flex-wrap gap-1 mt-4 gap-y-5">
+          {filteredPages.map(
+            ({
+              id,
+              properties: {
+                Link: { url },
+                Name: { title },
+                Tags,
+              },
+            }) => {
+              return (
+                <UrlItem
+                  key={id}
+                  url={url}
+                  title={title
+                    .map(({ plain_text }: any) => plain_text)
+                    .join(' ')}
+                />
+              );
+            }
+          )}
+        </div>
+      )}
+      {viewMode === 'table' && (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Link</TableHead>
+              <TableHead>Tags</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {filteredPages.map(
+              ({
+                id,
+                properties: {
+                  Link: { url },
+                  Name: { title },
+                  Tags,
+                },
+              }) => {
+                return (
+                  <TableRow key={id}>
+                    <TableCell>
+                      {title.map(({ plain_text }: any) => plain_text).join(' ')}
+                    </TableCell>
+                    <TableCell>
+                      <a
+                        href={url}
+                        target="_blank"
+                        className="text-sm text-black dark:text-gray-200 truncate"
+                        rel="noreferrer"
+                      >
+                        {url}
+                      </a>
+                    </TableCell>
+                    <TableCell className="flex gap-2">
+                      {Tags.multi_select.map(({ name, color }: any) => (
+                        <Badge
+                          key={name}
+                          className="cursor-pointer"
+                          style={{ backgroundColor: color }}
+                        >
+                          {name}
+                        </Badge>
+                      ))}
+                    </TableCell>
+                  </TableRow>
+                );
+              }
+            )}
+          </TableBody>
+        </Table>
+      )}
     </div>
   );
 }
